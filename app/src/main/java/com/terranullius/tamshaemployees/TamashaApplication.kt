e0b6b497f807548b1ba3f9b0649d519ea2d84f74
Shaique <esska099@gmail.com>
2022-09-02 21:41:14 +0530

    set up DI

---
+++ app/src/main/java/com/terranullius/tamshaemployees/TamashaApplication.kt
@@ -0,0 +1,7 @@
+package com.terranullius.tamshaemployees
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class TamashaApplication: Application()
---
+++ app/src/main/java/com/terranullius/tamshaemployees/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.terranullius.tamshaemployees.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun providesRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl("https://jsonplaceholder.typicode.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
